3b4135457b90255fcc422329f48f1e7f086ff4de
Newmaster-70 <rliu2000@outlook.com>
2022-01-29 10:04:03 -0600

    update step size

--- Euler's_Formula_Step_Size.cpp
+++ Euler's_Formula_Step_Size.cpp
@@ -15,7 +15,7 @@ float x0= 1;           //inital value of x; change this line!!!!             ===
 //                                                                           ===
 float x1= 2;           //end value of x; change this line!!!!                ===
 //                                                                           ===
-float dx= 0.25;        //put in step size                                    ===
+float dx= 0.25;        //put in step size; change this line!!!!              ===
 //==============================================================================
 int h= (x1 - x0)/dx;            // number of steps; change this line!!!!
 float dydx;
